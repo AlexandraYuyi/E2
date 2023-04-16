b115e0b8e3e596a2a492d925daa515a6a92c911e
Yuyi <alexandrayuyi@gmail.com>
2023-04-16 16:54:45 -0400

    interfaz hecha

--- src/interfaz/Interfaz.java
+++ src/interfaz/Interfaz.java
@@ -16,38 +16,48 @@ import javax.swing.JTextArea;
 import java.math.BigInteger;
 
 public class Interfaz extends JFrame implements ActionListener {
-    private JTextField input; 
+    
     private JLabel enunciado;
     private JLabel warning;
     private JLabel warning2;
+    
+    private JTextField input;
+    private JTextField input2;
     private JButton calcular;
     private JButton limpiar;
-    private JLabel matrizLabel;
-    private JLabel ordMayorMenor;
-    private JLabel multColUno;
-    private JLabel numInvertido;
-    private JLabel promDiagSec;
+    
+    private JLabel primoLabel;
+    private JTextArea resultadoPrimo;
+    
     private JLabel potencia;
-    private JTextArea resultadoOrden;
-    private JTextArea resultadoMult;
-    private JTextArea resultadoInvert;
-    private JTextArea resultadoProm;
-    private JTextArea resultadoPot;
+    private JTextArea resultPotencia;
     private JScrollPane scrollpane1;
-    private JTextArea matrizCuadro;
+    
+    private JLabel fibonacci;
+    private JTextArea resultFibonacci;
+    private JScrollPane scrollpane2;
+    
+    private JLabel factorial;
+    private JTextArea resultFactorial;
+    private JScrollPane scrollpane3;
+    
+    private JLabel suma;
+    private JTextArea resultSuma;
+    
+    
     
     public Interfaz(){
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setTitle("Evaluación 1 de Algoritmos y Estructuras I");
+        setTitle("Evaluación 2 de Algoritmos y Estructuras I");
         
-        enunciado = new JLabel("Ingrese un número del 1 al 9: ");
-        enunciado.setBounds(331,30,480,30);
+        enunciado = new JLabel("Ingrese dos números m y n del -99 al 99 ");
+        enunciado.setBounds(40,30,555,30);
         enunciado.setFont(new Font("Verdana", Font.BOLD, 24));
         add(enunciado);
         
         input = new JTextField();
-        input.setBounds(741,25,40,40);
+        input.setBounds(610,25,40,40);
         input.setFont(new Font("Verdana", Font.BOLD, 24));
         
         input.addKeyListener(new KeyAdapter() {
@@ -57,6 +67,18 @@ public class Interfaz extends JFrame implements ActionListener {
         });
         
         add(input);
+        
+        input2 = new JTextField();
+        input2.setBounds(670,25,40,40);
+        input2.setFont(new Font("Verdana", Font.BOLD, 24));
+        
+        input2.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                inputKeyTyped(e);
+            }
+        });
+        
+        add(input2);
         
         warning = new JLabel("Por favor ingrese un numero válido...");
         warning.setBounds(331,70,480,30);
@@ -75,85 +97,92 @@ public class Interfaz extends JFrame implements ActionListener {
         add(warning2);
         
         calcular = new JButton("Calcular");
-        calcular.setBounds(803,25,150,40);
+        calcular.setBounds(740,25,150,40);
         calcular.setFont(new Font("Verdana", Font.BOLD, 18));
         add(calcular);
         calcular.addActionListener(this);
         
         limpiar = new JButton("Limpiar");
-        limpiar.setBounds(980,25,150,40);
+        limpiar.setBounds(920,25,150,40);
         limpiar.setFont(new Font("Verdana", Font.BOLD, 18));
         limpiar.setEnabled(false);
         add(limpiar);
         limpiar.addActionListener(this);
         
-        matrizLabel = new JLabel("Matriz:");
-        matrizLabel.setBounds(40,115,480,30);
-        matrizLabel.setFont(new Font("Verdana", Font.BOLD, 20));
-        add(matrizLabel);
         
-        ordMayorMenor = new JLabel("Elementos ordenados de mayor a menor: ");
-        ordMayorMenor.setBounds(743,115,480,30);
-        ordMayorMenor.setFont(new Font("Verdana", Font.BOLD, 20));
-        add(ordMayorMenor);
         
-        multColUno = new JLabel("Multiplicación de la 1era columna: ");
-        multColUno.setBounds(743,310,480,30);
-        multColUno.setFont(new Font("Verdana", Font.BOLD, 20));
-        add(multColUno);
+        primoLabel = new JLabel("M es primo:");
+        primoLabel.setBounds(40,120,150,30);
+        primoLabel.setFont(new Font("Verdana", Font.BOLD, 20));
+        add(primoLabel);
         
-        numInvertido = new JLabel("Número del resultado anterior invertido: ");
-        numInvertido.setBounds(743,400,480,30);
-        numInvertido.setFont(new Font("Verdana", Font.BOLD, 20));
-        add(numInvertido);
+        resultadoPrimo = new JTextArea();
+        resultadoPrimo.setBounds(40,160,300,50);
+        resultadoPrimo.setFont(new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 18));
+        resultadoPrimo.setEditable(false);
+        add(resultadoPrimo);
         
-        promDiagSec = new JLabel("Promedio de la diagonal secundaria: ");
-        promDiagSec.setBounds(743,490,480,30);
-        promDiagSec.setFont(new Font("Verdana", Font.BOLD, 20));
-        add(promDiagSec);
         
-        potencia = new JLabel("Potencia del menor elemento positivo elevado al mayor elemento: ");
-        potencia.setBounds(40,605,800,30);
+        
+        suma = new JLabel("Suma de los dígitos |nxm|:");
+        suma.setBounds(400,120,480,30);
+        suma.setFont(new Font("Verdana", Font.BOLD, 20));
+        add(suma);
+        
+        resultSuma = new JTextArea();
+        resultSuma.setBounds(400,160,300,50);
+        resultSuma.setFont(new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 18));
+        resultSuma.setEditable(false);
+        add(resultSuma);
+        
+        
+        
+        potencia = new JLabel("Potencia n^m:");
+        potencia.setBounds(40,260,480,30);
         potencia.setFont(new Font("Verdana", Font.BOLD, 20));
         add(potencia);
         
-        resultadoOrden = new JTextArea();
-        resultadoOrden.setBounds(743,155,700,140);
-        resultadoOrden.setFont(new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 18));
-        resultadoOrden.setEditable(false);
-        add(resultadoOrden);
-        
-        resultadoMult = new JTextArea();
-        resultadoMult.setBounds(743,345,700,40);
-        resultadoMult.setFont(new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 18));
-        resultadoMult.setEditable(false);
-        add(resultadoMult);
-        
-        resultadoInvert = new JTextArea();
-        resultadoInvert.setBounds(743,435,700,40);
-        resultadoInvert.setFont(new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 18));
-        resultadoInvert.setEditable(false);
-        add(resultadoInvert);
-        
-        resultadoProm = new JTextArea();
-        resultadoProm.setBounds(743,525,700,40);
-        resultadoProm.setFont(new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 18));
-        resultadoProm.setEditable(false);
-        add(resultadoProm);
-        
-        resultadoPot = new JTextArea();
-        resultadoPot.setEditable(false);
-        resultadoPot.setFont(new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 18));
-        add(resultadoPot);
-        scrollpane1 = new JScrollPane(resultadoPot);
-        scrollpane1.setBounds(40,640,1405,50);
+        resultPotencia = new JTextArea();
+        resultPotencia.setFont(new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 18));
+        resultPotencia.setEditable(false);
+        add(resultPotencia);
+        scrollpane1 = new JScrollPane(resultPotencia);
+        scrollpane1.setBounds(40,300,1032,50);
         add(scrollpane1);
         
-        matrizCuadro = new JTextArea();
-        matrizCuadro.setBounds(40,155,660,400);
-        matrizCuadro.setFont(new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 18));
-        matrizCuadro.setEditable(false);
-        add(matrizCuadro);
+        
+        
+        fibonacci = new JLabel("Secuencia de Fibonacci hasta el término menor o igual a |nxm|: ");
+        fibonacci.setBounds(40,400,750,30);
+        fibonacci.setFont(new Font("Verdana", Font.BOLD, 20));
+        add(fibonacci);
+        
+        resultFibonacci = new JTextArea();
+        resultFibonacci.setFont(new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 18));
+        resultFibonacci.setEditable(false);
+        add(resultFibonacci);
+        scrollpane2 = new JScrollPane(resultFibonacci);
+        scrollpane2.setBounds(40,440,1032,50);
+        add(scrollpane2);
+        
+        
+        
+        
+        factorial = new JLabel("Factorial n!: ");
+        factorial.setBounds(40,540,480,30);
+        factorial.setFont(new Font("Verdana", Font.BOLD, 20));
+        add(factorial);
+        
+        resultFactorial = new JTextArea();
+        resultFactorial.setFont(new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 18));
+        resultFactorial.setEditable(false);
+        add(resultFactorial);
+        scrollpane3 = new JScrollPane(resultFactorial);
+        scrollpane3.setBounds(40,580,1032,50);
+        add(scrollpane3);
+        
+        
+          
     }
     
     @Override
@@ -180,16 +209,16 @@ public class Interfaz extends JFrame implements ActionListener {
                     BigInteger potencia = new BigInteger("1");
                     
                     matriz = Logica.llenarMatriz(matriz);
-                    matrizCuadro.setText(Logica.mostrarMatriz(matriz));
+                    resultadoPrimo.setText(Logica.mostrarMatriz(matriz));
                     vector = Logica.ordenarMatriz(matriz);
-                    resultadoOrden.setText(Logica.mostrarVector(vector));
-                    resultadoMult.setText(String.valueOf(Logica.multiplicarColumna(matriz)));
+                    resultPotencia.setText(Logica.mostrarVector(vector));
+                    resultFibonacci.setText(String.valueOf(Logica.multiplicarColumna(matriz)));
                     escalar = Logica.multiplicarColumna(matriz);
-                    resultadoInvert.setText(Logica.invertirNumero(escalar));
+                    resultFactorial.setText(Logica.invertirNumero(escalar));
                     real = Logica.promedioSumaDiagonalSecundaria(matriz);
-                    resultadoProm.setText(Float.toString(real));
+                    resultSuma.setText(Float.toString(real));
                     potencia = Logica.potenciaMenorMayor(vector);
-                    resultadoPot.setText(String.valueOf(potencia));
+//                    resultadoPot.setText(String.valueOf(potencia));
                     
                     }else{
                         warning.setVisible(true);
@@ -206,18 +235,18 @@ public class Interfaz extends JFrame implements ActionListener {
             calcular.setEnabled(true);
             input.setEditable(true);
             input.setText(null);
-            matrizCuadro.setText("");
-            resultadoOrden.setText("");
-            resultadoMult.setText("");
-            resultadoInvert.setText("");
-            resultadoProm.setText("");
-            resultadoPot.setText("");
+            resultadoPrimo.setText("");
+            resultPotencia.setText("");
+            resultFibonacci.setText("");
+            resultFactorial.setText("");
+            resultSuma.setText("");
+//            resultadoPot.setText("");
         }
     }
     
     public static void main(String[] args) {
         Interfaz interfaz = new Interfaz();
-        interfaz.setBounds(0,0,1500,770);
+        interfaz.setBounds(0,0,1115,700);
         interfaz.setResizable(false);
         interfaz.setVisible(true);
         interfaz.setLocationRelativeTo(null);
@@ -225,7 +254,7 @@ public class Interfaz extends JFrame implements ActionListener {
     
     private void inputKeyTyped(KeyEvent e){
         char tecla= e.getKeyChar();
-        if(!(Character.isDigit(tecla)) || (tecla == KeyEvent.VK_BACK_SPACE) || (tecla == KeyEvent.VK_DELETE)){
+        if((!(Character.isDigit(tecla)) && tecla != '-') || (tecla == KeyEvent.VK_BACK_SPACE) || (tecla == KeyEvent.VK_DELETE)){
             e.consume();
             }
     }
